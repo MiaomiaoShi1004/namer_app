f6c7ccd92176f4a070a6a4dccca507bb8b25d88a
MiaomiaoShi1004 <simoneshi1004@outlook.com>
2024-04-09 22:50:27 +0200

    Make the app prettier

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 32, 83, 125)),
         ),
         home: MyHomePage(),
       ),
@@ -38,24 +38,56 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random AWESOME idea:'),
-          Text(appState.current.asLowerCase),
-          Text(appState.current.asSnakeCase),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Text('A random AWESOME idea:'),
+            BigCard(pair: pair),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+                // print('I pressed a button!');
+              },
+              child: Text('Next'),
+            ),
+        
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-          // Add a button
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-              // print('I pressed a button!');
-            },
-            child: Text('Next'),
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.secondary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          pair.asPascalCase, 
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
           ),
-
-        ],
       ),
     );
   }
